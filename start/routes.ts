5df96b37eee591a38b3346649377e862eb744d81
Adam <aj.murphy@outlook.com>
2025-08-10 19:25:33 +0100

    feat: define API routes for todays word and generation

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,15 @@
 |
 */
 
+const TodaysController = () => import('#controllers/todays_controller')
+const GenerateController = () => import('#controllers/generate_controller')
 import router from '@adonisjs/core/services/router'
+
+router
+  .group(() => {
+    router.get('/word', [TodaysController])
+    router.post('/generate', [GenerateController])
+  })
+  .prefix('/api')
 
 router.on('/').render('pages/home')
