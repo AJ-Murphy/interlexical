6a93e1c8ce9d3a68eab3c3a1a8b2bae26281b227
Adam <aj.murphy@outlook.com>
2025-09-13 13:22:53 +0100

    feat: configure PostgreSQL database connection and update environment variables

--- config/database.ts
+++ config/database.ts
@@ -1,13 +1,30 @@
+import env from '#start/env'
 import app from '@adonisjs/core/services/app'
 import { defineConfig } from '@adonisjs/lucid'
 
 const dbConfig = defineConfig({
-  connection: 'sqlite',
+  connection: 'pg',
+  prettyPrintDebugQueries: true,
   connections: {
+    pg: {
+      client: 'postgres',
+      connection: {
+        host: env.get('PG_HOST'),
+        user: env.get('PG_USER'),
+        port: env.get('PG_PORT'),
+        password: env.get('PG_PASSWORD'),
+        database: env.get('PG_NAME'),
+        ssl: true,
+      },
+      migrations: {
+        naturalSort: true,
+        paths: ['database/migrations'],
+      },
+    },
     sqlite: {
       client: 'better-sqlite3',
       connection: {
-        filename: app.tmpPath('db.sqlite3')
+        filename: app.tmpPath('db.sqlite3'),
       },
       useNullAsDefault: true,
       migrations: {
--- start/env.ts
+++ start/env.ts
@@ -25,4 +25,14 @@ export default await Env.create(new URL('../', import.meta.url), {
   |----------------------------------------------------------
   */
   SESSION_DRIVER: Env.schema.enum(['cookie', 'memory'] as const),
+
+  PG_HOST: Env.schema.string(),
+
+  PG_NAME: Env.schema.string(),
+
+  PG_USER: Env.schema.string(),
+
+  PG_PASSWORD: Env.schema.string(),
+
+  PG_PORT: Env.schema.number()
 })
