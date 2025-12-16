19635d58fbd1e501be6596f061e7f1ecb4805311
Adam <aj.murphy@outlook.com>
2025-12-16 15:12:47 +0000

    chore: removed unused api route

--- start/routes.ts
+++ start/routes.ts
@@ -10,10 +10,4 @@
 const TodaysController = () => import('#controllers/todays_controller')
 import router from '@adonisjs/core/services/router'
 
-router
-  .group(() => {
-    router.get('/word', [TodaysController, 'find'])
-  })
-  .prefix('/api')
-
 router.get('/', [TodaysController, 'show'])
